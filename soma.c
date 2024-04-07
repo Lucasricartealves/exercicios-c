b27c5a164f9b0b25d8f5cc88c4e41bfe72901244
Lucasricartealves <165337836+Lucasricartealves@users.noreply.github.com>
2024-04-06 21:32:00 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int
+main ()
+{
+  int sum1 = 50 + 30;
+  printf ("%d/n", sum1);
+
+  int sum2 = sum1 + 90;
+  printf ("%d/n", sum2);
+
+  int sum3 = sum2 + sum2;
+  printf ("%d/n", sum3);
+
+  return 0;
+}
